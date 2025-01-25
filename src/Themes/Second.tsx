b0d4fc97585ff87ce152eed8e3d9113f60ceec52
최원빈 <wonbin109@naver.com>
2025-01-25 19:59:33 +0900

    <append> Theme.2

---
+++ src/Themes/Second.tsx
@@ -0,0 +1,14 @@
+const Second = () => {
+  const twoOrOne: number | string = 'kim';
+  const objectTwo: { src: number | string };
+  const arrayTwo: number[] | string[];
+
+  const 아무거나: any;
+  let safer: unknown;
+  safer = 1;
+
+  //any는 타입실드 해제라 타입 선언의 관점이 아닌 타입 선언의 관점임
+  return <div></div>;
+};
+
+export default Second;

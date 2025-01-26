321e094b63cbacadc9c47e0896f15724021f5b78
최원빈 <wonbin109@naver.com>
2025-01-26 15:44:53 +0900

    <Append> theme.3

--- src/Themes/Second.tsx
+++ src/Themes/Second.tsx
@@ -7,11 +7,19 @@ const Second = () => {
   let safer: unknown;
   safer = 1;
 
-
-
   //any는 타입실드 해제라 타입 선언의 관점이 아닌 타입 선언의 관점임
 
-  
+  function hi(x?: string) {
+    if (x === undefined) {
+      console.log('이름이 없네요');
+    } else {
+      console.log('홍길동임');
+    }
+  }
+
+  hi();
+  hi('하이');
+
   return <div></div>;
 };
 
